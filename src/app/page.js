7f79b84c98ed03b40d38405a3f70ecdc59b81219
elaf <elafghassangardi@gmail.com>
2024-08-12 03:09:40 +0300

    chore(structure): move sidebar to components folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import SideBar from "@/components/sidebar/SideBar";
+
 export default function Home() {
   return (
-   <></>
+   <main className="bg-background w-full min-h-screen">
+    <SideBar/>
+   </main>
   );
 }
