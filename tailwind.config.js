5706c1b496b134cf11b2696e332ed2bed2cf412e
elaf <elafghassangardi@gmail.com>
2024-08-12 05:16:29 +0300

    feat(ui): add fonts ( Bebas Neue, Roboto) to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'selector',
+  darkMode: "selector",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,21 +8,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        bebas: ["Bebas Neue", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"]
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        background:"rgba(var(--background))",
-        light_pink:'rgba(var( --light_pink))',
-        primary_pink:'rgba(var(--primary_pink))',
-        light_blue:'rgba(var(--light_blue))',
-        primary_blue:'rgba(var(--primary_blue))',
-        light_green:'rgba(var(--light_green))',
-        primary_green:'rgba(var(--primary_green))'
-      }
+      colors: {
+        background: "#e1e1e1",
+        primary_orange: "#FDBA74",
+        primary_blue: "#57708D"
+      },
     },
   },
   plugins: [],
-};
+}
