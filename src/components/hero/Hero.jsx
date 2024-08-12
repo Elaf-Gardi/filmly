184060b603dcedbcc84e5d538410afb72480b1f3
elaf <elafghassangardi@gmail.com>
2024-08-12 05:17:26 +0300

    feat: create hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import SideBar from '../sidebar/SideBar'
+const Hero = () => {
+  return (
+    <div className='bg-background h-screen'>
+    <SideBar/>
+    </div>
+  )
+}
+export default Hero
