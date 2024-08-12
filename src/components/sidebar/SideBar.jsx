1b6ec68a09302b620166d016b38ca36bec399bb2
elaf <elafghassangardi@gmail.com>
2024-08-12 03:09:12 +0300

    chore(structure): move sidebar to components folder

---
+++ src/components/sidebar/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { IoHome } from "react-icons/io5"
+import { BsBookmarkHeartFill } from "react-icons/bs"
+
+const SideBar = () => {
+  return (
+    <>
+      <div>
+        <IoHome />
+        <BsBookmarkHeartFill />
+      </div>
+    </>
+  )
+}
+
+export default SideBar
