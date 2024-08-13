09e9303c0fb8d665aaaa451b50ab0379774a06f6
elaf <elafghassangardi@gmail.com>
2024-08-13 06:18:58 +0300

    feat: add watch trailer button

--- src/components/ui/WatchTrailerButton.jsx
+++ src/components/ui/WatchTrailerButton.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { FaArrowRight } from "react-icons/fa";
 
 const WatchTrailerButton = () => {
   return (
-    <div>WatchTrailerButton</div>
-  )
+    <button className="flex items-center justify-center py-2 px-6 w-full h-12 rounded-lg text-white font-medium gap-2 bg-[#57708D] hover:bg-[#3F3D56] relative z-20">
+      Watch Trailer <FaArrowRight className="text-white text-lg" />
+    </button>
+  );
 }
 
-export default WatchTrailerButton
+export default WatchTrailerButton;
