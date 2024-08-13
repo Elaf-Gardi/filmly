ba607ef8a8c10fbc6ebc462f797dd2af4a57db41
elaf <elafghassangardi@gmail.com>
2024-08-13 06:18:31 +0300

    feat: add favorite button

--- src/components/ui/FavoriteButton.jsx
+++ src/components/ui/FavoriteButton.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { GoHeartFill } from "react-icons/go";
+import React from "react"
+import { GoHeartFill } from "react-icons/go"
 
 const FavoriteButton = () => {
   return (
-    <button className='flex items-center justify-center w-12 h-12 rounded-lg bg-red-400/50 hover:bg-red-400/60 relative z-20'><GoHeartFill className='text-red-500 text-xl'/></button>
+    <button className="flex items-center justify-center w-12 h-12 rounded-lg bg-red-400/50 hover:bg-red-400/60 relative z-20">
+      <GoHeartFill className="text-red-500 text-xl" />
+    </button>
   )
 }
 
