f14055b7bc471d314a9f77d988ecd4347f2a5dc1
elaf <elafghassangardi@gmail.com>
2024-08-13 04:52:52 +0300

    feat: re-layout hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import MovieList from "../movies/MovieList"
+import SearchBar from "../searchBar/SearchBar"
 const Hero = () => {
   return (
-    <>
-     <MovieList/>
-    </>
+    <main className="flex flex-col lg:ml-60 px-10">
+      <SearchBar />
+      <MovieList />
+    </main>
   )
 }
 export default Hero
