6398057a4c7574446e72373ee8c7f4952e0d7ed6
elaf <elafghassangardi@gmail.com>
2024-08-13 04:09:35 +0300

    chore(deps): remove unused @butterfail/tailwindcss-inverted-radius plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {
       fontFamily: {
         bebas: ["Bebas Neue", "sans-serif"],
-        roboto: ["Roboto", "sans-serif"]
+        roboto: ["Roboto", "sans-serif"],
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -19,11 +19,11 @@ module.exports = {
       },
       colors: {
         primary_blue: "#57708D",
-        dark_blue: '#2D2F3B',
-        light_purple: '#6C63FF',
-        dark_purple: '#3F3D56',
-        dark_gray: '#24252D',
-        light_gray: '#8C8C8E',
+        dark_blue: "#2D2F3B",
+        light_purple: "#6C63FF",
+        dark_purple: "#3F3D56",
+        dark_gray: "#24252D",
+        light_gray: "#8C8C8E",
       },
     },
   },
