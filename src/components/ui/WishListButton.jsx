cccf667761e50dc65931afcac8b882d92e1cf3f9
Elaf Gardi <111548041+Elaf-Gardi@users.noreply.github.com>
2024-08-15 13:06:24 +0300

    feat: update WishListButton.jsx

--- src/components/ui/WishListButton.jsx
+++ src/components/ui/WishListButton.jsx
@@ -3,7 +3,7 @@ import { FaPlus } from "react-icons/fa"
 
 const WishListButton = () => {
   return (
-    <button className="flex items-center justify-center w-12 h-12 rounded-lg bg-primary_gray hover:bg-primary_gray/40 relative z-20">
+    <button className="flex items-center justify-center w-16 h-12 rounded-lg bg-primary_gray hover:bg-primary_gray/40 relative z-20">
       <FaPlus className="text-primary_green text-xl" />
     </button>
   )
